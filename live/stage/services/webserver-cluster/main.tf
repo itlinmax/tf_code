99177450fbac38c74c934580d74ea67f571f65dc
itlinmax <itlinmax@gmail.com>
2024-12-15 20:11:57 +0200

    chapter 5

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
     region = "eu-central-1"
+    default_tags {
+        tags = {
+            Owner = "team-foo"
+            ManagedBy = "Terraform"
+        }
+    }
 }
 module "webserver_cluster" {
     source = "../../../../modules/services/webserver-cluster"
@@ -8,23 +14,13 @@ module "webserver_cluster" {
     instance_type = "t4g.nano"
     min_size = 2
     max_size = 2
+    enable_autoscaling = false
+    custom_tags = {
+        Owner = "team-foo"
+        ManagedBy = "terraform"
+    }
 }
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-    scheduled_action_name = "scale-out-during-business-hours"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 10
-    recurrence = "0 9 * * *"
-    autoscaling_group_name = module.webserver_cluster.asg_name
-}
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-    scheduled_action_name = "scale-in-at-night"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 2
-    recurrence = "0 17 * * *"
-    autoscaling_group_name = module.webserver_cluster.asg_name
-} 
+
 resource "aws_security_group_rule" "allow_testing_inbound" {
     type = "ingress"
     security_group_id = module.webserver_cluster.alb_security_group_id
--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -40,6 +40,14 @@ resource "aws_autoscaling_group" "example" {
         value = "${var.cluster_name}-aws-autoscaling-group"
         propagate_at_launch = true
     }
+    dynamic "tag" {
+    for_each = var.custom_tags
+        content {
+            key = tag.key
+            value = tag.value
+            propagate_at_launch = true
+        }
+    }
     launch_template {
         id      = aws_launch_template.example.id
         version = "$Latest"
@@ -143,4 +151,23 @@ resource "aws_lb_listener_rule" "asg" {
         type = "forward"
         target_group_arn = aws_lb_target_group.asg.arn
     }
+}
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+    count = var.enable_autoscaling ? 1 : 0
+    scheduled_action_name = "${var.cluster_name}-scale-out-during- business-hours"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 10
+    recurrence = "0 9 * * *"
+    autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+    count = var.enable_autoscaling ? 1 : 0
+    scheduled_action_name = "${var.cluster_name}-scale-in-at- night"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 2
+    recurrence = "0 17 * * *"
+    autoscaling_group_name = aws_autoscaling_group.example.name
 }
